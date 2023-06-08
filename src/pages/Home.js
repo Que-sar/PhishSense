fc7917bbb22c01fe9e410f98f5d11640274e9ca0
Que-sar <arato.viktor@gmail.com>
2023-06-08 16:16:44 +0100

    Hero page beginnings, lady image and titles, description, call to action button, NO STYLING YET

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,37 @@
+import React from 'react';
 import "./Home.css";
+import HeroImage from "../components/images/heroImage.png";
 
 const Home = () => {
-    return <>
-    <div className="holder"></div>
-    <div className="example">
-    <h1>Home Page</h1>
-    </div>  
-    </>  
-}
+
+return (
+<div className='home-container'>
+    <div className='hero-section'>
+        <div className='hero-action'>
+            <div className='hero-title'>
+                <h1>Title</h1>
+            </div>
+            <div className='hero-description'>
+                <h3>I am the description</h3>
+                </div>
+            <div className='hero-call-to-action'>
+                <button>Call</button>
+                </div>    
+        </div>
+        <div className='hero-image'>
+            <img src={HeroImage} alt='Hero'/>
+            </div>
+    </div>
+    <div className='benefits-section'>
+        <div className='firstBenefit'>One</div>
+        <div className='secondBenefit'>Two</div>
+        <div className='thirdBenefit'>Three</div>
+    </div>
+</div>
+
+
+)
+
+    };
 
 export default Home;

a28fe512d1bb4ceb4ece66eb69e1f35a5d3d92af
Que-sar <arato.viktor@gmail.com>
2023-07-04 20:58:34 +0100

    Hover demo text, changed hero image, began on more info panel

--- src/components/simulation/SimulationPanel.js
+++ src/components/simulation/SimulationPanel.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./SimulationPanel.css";
+import MoreInfo from "./MoreInfo";
 
 import GiveUp from "../images/simulationIconsSmaller/giveup.svg";
 import Report from "../images/simulationIconsSmaller/report.svg";
+import MarkReport from "../images/simulationIconsSmaller/markReport.svg";
 import Delete from "../images/simulationIconsSmaller/delete.svg";
 import Reply from "../images/simulationIconsSmaller/reply.svg";
 import Forward from "../images/simulationIconsSmaller/forward.svg";
@@ -13,21 +15,37 @@ import ProfilePic from "../images/simulationIconsSmaller/profilePic.svg";
 
 const SimulationPanel = (props) => {
   const [flagged, setFlagged] = useState(false);
+  const [marked, setMarked] = useState(false);
+  const [legitMarked, setLegitMarked] = useState(false);
+  const [maliciousMarked, setMaliciousMarked] = useState(false);
+
   const [legitimateColor, setLegitimateColor] = useState("#f5f5f5");
   const [maliciousColor, setMaliciousColor] = useState("#f5f5f5");
 
   const displayText = props.EmailDisplay;
+
+  const moreClick = () => {
+    return <MoreInfo />;
+  };
+
+  const markClick = () => {
+    setMarked(!marked);
+  };
 
   const flagClick = () => {
     setFlagged(!flagged);
   };
 
   const legitimateClick = () => {
+    setMaliciousMarked(false);
+    setLegitMarked(true);
     setMaliciousColor("#f5f5f5");
     setLegitimateColor("#1ED44B");
   };
 
   const maliciousClick = () => {
+    setLegitMarked(false);
+    setMaliciousMarked(true);
     setLegitimateColor("#f5f5f5");
     setMaliciousColor("#DE2626");
   };
@@ -38,6 +56,7 @@ const SimulationPanel = (props) => {
           <div className="icon done-button">
             <button className="giveup-click">
               <img src={GiveUp} alt="GiveUp" />
+              <span className="tooltiptext-giveup">Mark as finished</span>
             </button>
           </div>
           <div className="small-line"></div>
@@ -50,6 +69,9 @@ const SimulationPanel = (props) => {
               >
                 <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
               </svg>
+              <span className="tooltiptext-legitimate">
+                {legitMarked ? "Marked as legitimate" : "Mark as legitimate"}
+              </span>
             </button>
           </div>
           <div className="icon markbad-phishing-button">
@@ -61,17 +83,24 @@ const SimulationPanel = (props) => {
               >
                 <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
               </svg>
+              <span className="tooltiptext-malicious">
+                {maliciousMarked ? "Marked as malicious" : "Mark as malicious"}
+              </span>
             </button>
           </div>
           <div className="small-line"></div>
           <div className="icon report-phishing-button">
-            <button className="report-click">
-              <img src={Report} alt="Report" />
+            <button onClick={markClick} className="report-click">
+              <img src={marked ? Report : MarkReport} alt="Report" />
+              <span className="tooltiptext-report">
+                {marked ? "Marked as spam" : "Mark as spam"}
+              </span>
             </button>
           </div>
           <div className="icon delete-button">
             <button className="delete-click">
               <img src={Delete} alt="Delete" />
+              <span className="tooltiptext-delete">Delete</span>
             </button>
           </div>
 
@@ -79,11 +108,13 @@ const SimulationPanel = (props) => {
           <div className="icon reply-button">
             <button className="reply-click">
               <img src={Reply} alt="Reply" />
+              <span className="tooltiptext-reply">Reply</span>
             </button>
           </div>
           <div className="icon forward-button">
             <button className="forward-click">
               <img src={Forward} alt="Forward" />
+              <span className="tooltiptext-forward">Forward</span>
             </button>
           </div>
         </div>
@@ -112,11 +143,15 @@ const SimulationPanel = (props) => {
             <div className="icon flag-button">
               <button onClick={flagClick} className="flag-click">
                 <img src={flagged ? DarkFlag : LightFlag} alt="Flag" />
+                <span className="tooltiptext-flag">
+                  {flagged ? "Flagged" : "Flag it"}
+                </span>
               </button>
             </div>
             <div className="icon more-actions-button">
-              <button className="more-click">
+              <button className="more-click" onClick={moreClick}>
                 <img src={More} alt="Show more" />
+                <span className="tooltiptext-more">More information</span>
               </button>
             </div>
           </div>
@@ -125,11 +160,13 @@ const SimulationPanel = (props) => {
           <div className="reply-button">
             <button className="reply-click bottom">
               <img src={Reply} alt="Reply" className="reply-icon-down" />
+              <span className="tooltiptext-reply">Reply</span>
             </button>
           </div>
           <div className="forward-button">
             <button className="forward-click bottom">
               <img src={Forward} alt="Forward" />
+              <span className="tooltiptext-forward">Forward</span>
             </button>
           </div>
         </div>

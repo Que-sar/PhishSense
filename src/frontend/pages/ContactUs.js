e5aed7dfec614b21b47a1524da8189e345b1d465
ignotus <arato.viktor@gmail.com>
2024-01-23 01:56:37 +0000

    made the background requirements to work on the Contact Us page

--- src/frontend/pages/ContactUs.js
+++ src/frontend/pages/ContactUs.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Footer from "../components/footer/Footer";
+import "../components/contact/ContactUs.css";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <div className="contact-us-page-container">
+      <div className="contact-us-boxes-container">
+        <div className="contact-us-details">
+          DD1 ASD
+          <br /> Dundee
+          <br /> etcDD1 ASD
+          <br />
+          <br />
+          <br /> Dundee
+          <br /> etc
+        </div>
+        <div className="contact-us-boxes"></div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;

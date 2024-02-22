aa5b62c875b73d680e7694b36141ae166c7ac3e0
ignotus <arato.viktor@gmail.com>
2024-02-22 22:05:21 +0000

    removed team's

--- src/frontend/components/home/Hero.js
+++ src/frontend/components/home/Hero.js
@@ -19,9 +19,9 @@ const Hero = () => {
           empower yourself.
         </h1>
         <div className="hero-description">
-          Enhance your team's resilience against cyber threats with our
-          personalized, affordable testing and education programs, aimed at
-          boosting your cybersecurity.
+          Enhance your resilience against cyber threats with our personalized,
+          affordable testing and education programs, aimed at boosting your
+          cybersecurity.
         </div>
         <div className="hero-call-to-action">
           <button className="hero-action-button" onClick={navigateToContact}>

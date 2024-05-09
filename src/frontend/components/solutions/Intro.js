d9f04163064c70a789aa752e944a77710893f40f
ignotus <arato.viktor@gmail.com>
2024-05-09 13:30:37 +0100

    Changes to solution with webapp added

--- src/frontend/components/solutions/Intro.js
+++ src/frontend/components/solutions/Intro.js
@@ -4,17 +4,16 @@ import "./Intro.css";
 const Intro = () => {
   return (
     <section className="solutions-introduction-container">
-      <h1 className="solutions-introduction-title">
-        Unlock Your Email Security
-      </h1>
+      <h1 className="solutions-introduction-title">Upgrade Your Security</h1>
       <div className="solutions-beginner-text">
-        In today's digital landscape, robust email security is crucial. Speculor
-        offers a suite of specialized phishing assessments and web application
-        testing to strengthen your organization's defenses. From Web-application
-        Testing and training to General Phishing, Spear Phishing or Whaling,
-        Quishing, and SMS Phishing, our services are designed to identify
-        vulnerabilities and bolster your protection. Embark on a journey with us
-        to enhance your cyber resilience and stay ahead of cyber threats.
+        In today's digital landscape, a robust security stance is crucial.
+        Speculor offers a suite of specialized web application testing and
+        phishing assessments to strengthen your organization's defenses. From
+        Web-application Testing and training to General Phishing, Spear Phishing
+        or Whaling, Quishing, and SMS Phishing, our services are designed to
+        identify vulnerabilities and bolster your protection. Embark on a
+        journey with us to enhance your cyber resilience and stay ahead of cyber
+        threats.
       </div>
     </section>
   );

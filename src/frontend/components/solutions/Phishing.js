61c9f2f0e01662cffc94920eba93740bdfb7b98b
ignotus <arato.viktor@gmail.com>
2024-07-22 00:29:11 +0100

    change in mission

--- src/frontend/components/solutions/Phishing.js
+++ src/frontend/components/solutions/Phishing.js
@@ -5,7 +5,9 @@ import PhishiMage from "../images/solutionimagery/phishing.webp";
 const Phishing = () => {
   return (
     <section className="phishing-phishing-container">
-      <div className="phishing-phishing-title">General Phishing Assessment</div>
+      <div className="phishing-phishing-title">
+        Comprehensive Phishing Assessment
+      </div>
       <div className="solutions-container-descriptions">
         <div className="solution-image-container">
           <img
@@ -17,11 +19,12 @@ const Phishing = () => {
         <div className="solution-text phishing-text-description">
           Equip your organization with the fundamental line of defense against
           phishing.
-          <br /> <br /> Our General Phishing Assessment tests your team's
+          <br /> <br /> Our Comprehensive Phishing Assessment tests your team's
           reflexes against broad-scale phishing attacks, employing simulated
-          phishing emails that lead to a safe but realistic landing page. <br />{" "}
-          <br /> We analyze the response and provide a detailed report to
-          highlight your security posture and areas for improvement. <br />
+          phishing emails, messages that lead to a safe but realistic landing
+          page. <br /> <br /> We analyze the response and provide a detailed
+          report to highlight your security posture and areas for improvement.{" "}
+          <br />
           <br />
           Following the assessment, we offer optional educational modules,
           tailored to your needs or test results, to reinforce your team's
